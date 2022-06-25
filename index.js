046c17fbeefa14a141afb09157c60ad5125e175b
Tutuezin <vinnycamara1@gmail.com>
2022-06-25 19:39:43 -0300

    :sparkles: Feat: posta mensagens

--- index.js
+++ index.js
@@ -4,26 +4,33 @@ import chalk from "chalk";
 import dotenv from "dotenv";
 import { MongoClient } from "mongodb";
 import joi from "joi";
+import dayjs from "dayjs";
 
+//CONFIGS
 dotenv.config();
 const server = express();
 server.use([cors(), express.json()]);
 
+//DATABASE
 const client = new MongoClient(process.env.MONGO_URI);
 let db;
-
 client.connect().then(() => {
   db = client.db("batePapoUol");
 });
 
+//SCHEMA
 const participantSchema = joi.object({
   name: joi.string().required(),
 });
 
-//TODO: Tirar essa variavel global e por no banco de dados
-// const participants = [];
+const messageSchema = joi.object({
+  to: joi.string().required(),
+  text: joi.string().required(),
+  type: joi.string().valid("private_message", "message").required(),
+});
 
-server.post("/participants", async (req, res) => {
+//PARTICIPANTS
+async function checkCadastre(req, res, next) {
   const participant = req.body;
 
   const validation = participantSchema.validate(participant, {
@@ -35,10 +42,34 @@ server.post("/participants", async (req, res) => {
     return;
   }
 
-  try {
-    participant.lastStatus = Date.now();
+  const participantExists = await db
+    .collection("participants")
+    .findOne({ name: participant.name });
 
+  if (participantExists) {
+    res.sendStatus(409);
+    return;
+  }
+
+  next();
+}
+
+server.post("/participants", checkCadastre, async (req, res) => {
+  const participant = req.body;
+
+  const enterRoom = {
+    from: participant.name,
+    to: "Todos",
+    text: "entra na sala...",
+    type: "status",
+    time: dayjs().format("HH:mm:ss"),
+  };
+
+  participant.lastStatus = Date.now();
+
+  try {
     await db.collection("participants").insertOne(participant);
+    await db.collection("messages").insertOne(enterRoom);
 
     res.sendStatus(201);
   } catch (error) {
@@ -52,6 +83,62 @@ server.get("/participants", async (req, res) => {
     const participants = await db.collection("participants").find().toArray();
 
     res.send(participants);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
+
+//MESSAGES
+
+async function checkMessage(req, res, next) {
+  const message = req.body;
+  const username = req.header("User");
+
+  const validation = messageSchema.validate(message, {
+    abortEarly: false,
+  });
+
+  if (validation.error) {
+    res.sendStatus(422);
+    return;
+  }
+
+  const participantExists = await db
+    .collection("participants")
+    .findOne({ name: username });
+
+  if (!participantExists) {
+    res.sendStatus(422);
+    return;
+  }
+
+  next();
+}
+
+server.post("/messages", checkMessage, async (req, res) => {
+  const message = req.body;
+  const username = req.header("User");
+
+  const sendMessage = {
+    from: username,
+    ...message,
+    time: dayjs().format("HH:mm:ss"),
+  };
+
+  await db.collection("messages").insertOne(sendMessage);
+
+  res.sendStatus(201);
+
+  console.log(username);
+});
+
+server.get("/messages", async (req, res) => {
+  const { limit } = req.query;
+  const messages = await db.collection("messages").find().toArray();
+
+  try {
+    res.send(messages);
   } catch (error) {
     console.error(error);
     res.sendStatus(500);
